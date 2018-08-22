d57891e285cb7adc6e0ebc04a7e2b8921c373933
Aziza <aziza.shestakova@nutnet.ru>
2018-08-22 21:39:38 +0300

    deep comparison

--- JSBasic/countingOfLetters.js
+++ JSBasic/countingOfLetters.js
@@ -1,12 +1,12 @@
-function countCharacters(str) {
-    var count = count.toLowerCase();
-    count = str.length;
+function countCharacters(str, symbol) {
+    str = str.toLowerCase();
+    var count = str.length;
     var result = 0;
-    for(i=0; i < count; i++) {
-        if(str.charAt(i) == " ") {
+    for(var i=0; i < count; i++) {
+        if(str.charAt(i) == symbol) {
             result++;
         }
-        result++;
     }
     return result;
 }
+console.log(countCharacters('My random string', 'm'));
---
+++ JSBasic/deepСomparison.js
@@ -0,0 +1,29 @@
+function deepCompare(object1, object2) {
+    var count1 = 0,
+        count2 = 0;
+	for (var key in object1) {
+		count1++;
+	}
+	for (var key in object2) {
+		count2++;
+	}
+	if (count1 != count2) {
+		return false;
+	}
+
+	for (var key in object1) {
+		if (object1[key] !== object2[key]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+console.log (deepCompare ( {one:1, two:`2`} , {one:1, two: `2`}));
+
+console.log (deepCompare ({one:1, two:`2`} , { two: 2}));
+
+console.log (deepCompare ({one:1, two:`2`} , { one:1, two: 2}));
+
+console.log (deepCompare ( {one:1, two:`2`} , { two: `2`, one:1 }));
